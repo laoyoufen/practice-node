1daf151e5153379e5c7fdf17637d1b0f4ceffffb
laoyoufen <liyoubing0503@163.com>
2016-12-16 18:12:17 +0800

    a

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-var express = require('express');
-var app = express();
 var path = require('path');
+var express = require('express');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var config = require('config-lite');
+var routes = require('./routes');
+var pkg = require('./package');
 
-/*app.set('views', path.join(__dirname, 'views'));
+var app = express();
+
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
 
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+// session 中间件
+app.use(session({
+  name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
+  secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  cookie: {
+    maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+  },
+  store: new MongoStore({// 将 session 存储到 mongodb
+    url: config.mongodb// mongodb 地址
+  })
+}));
+// flash 中间价，用来显示通知
+app.use(flash());
 
-app.use(() => {
-	console.log(1212)
-})
+// 路由
+routes(app);
 
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/user'));
-
-app.listen(3000)*/
-
-
-app.use(function(req, res, next) {
-  console.log('1');
-  next(new Error('haha'));
+// 监听端口，启动程序
+app.listen(config.port, function () {
+  console.log(`${pkg.name} listening on port ${config.port}`);
 });
-
-app.use(function(req, res, next) {
-  console.log('2');
-  res.status(200).end();
-});
-
-//错误处理
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-
-app.listen(3000)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 
-var path = require('path');
-var express = require('express');
-var session = require('express-session');
-
-
 module.exports = function (app) {
 	app.get('/', function(req, res){
 		res.redirect('/posts');
